7647285386730297f1c0e3efc58e7de2c2bf36e1
Dhananjay <dhananjayd@cybage.com>
2022-08-26 18:26:22 +0530

    feedback service updated

--- Backend/FoodAppFeedbackService/src/main/java/com/cybage/food/service/FeedbackService.java
+++
@@ -1,8 +0,0 @@
-package com.cybage.food.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class FeedbackService {
-
-}
--- Backend/OnlineFoodOrderingApp/src/main/java/com/cybage/food/service/UserService.java
+++ Backend/OnlineFoodOrderingApp/src/main/java/com/cybage/food/service/UserService.java
@@ -1,8 +1,15 @@
 package com.cybage.food.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-@Service
-public class UserService {
+import com.cybage.food.dao.UserRepository;
+import com.cybage.food.entity.User;
 
+@Service
+public class UserService{
+
+	@Autowired
+	private UserRepository userDao;
+	
 }
